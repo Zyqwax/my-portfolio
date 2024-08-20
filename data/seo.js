780e0d847617b28aedf610437ab1975576924ef3
Enes Samed <enessamed422@gmail.com>
2024-08-20 19:06:41 +0300

    Google index changes

--- data/seo.js
+++ data/seo.js
@@ -3,35 +3,35 @@ const SEO = [
     page: 'home',
     description:
       'Node.js konusunda uzman bir backend geliştiricisiyim. Çeşitli frameworkler ve teknolojiler kullanarak ölçeklenebilir, güvenli ve güvenilir web uygulamaları oluşturma deneyimim var.',
-    keywords: ['Tharindu', 'Tharindu N', 'Tharindu Nayanajith'],
+    keywords: ['Enes Samed Gözlü', 'Enes Samed', 'Enes'],
   },
 
   {
     page: 'about',
     description:
       'Node.js konusunda uzman bir backend geliştiricisiyim. Çeşitli frameworkler ve teknolojiler kullanarak ölçeklenebilir, güvenli ve güvenilir web uygulamaları oluşturma deneyimim var.',
-    keywords: ['Tharindu', 'Tharindu N', 'Tharindu Nayanajith'],
+    keywords: ['Enes Samed Gözlü', 'Enes Samed', 'Enes'],
   },
 
   {
     page: 'articles',
     description:
       'Programlama, liderlik, ürün tasarımı ve daha fazlası hakkında uzun vadeli düşüncelerimin kronolojik koleksiyonu.',
-    keywords: ['Tharindu', 'Tharindu N', 'Tharindu Nayanajith'],
+    keywords: ['Enes Samed Gözlü', 'Enes Samed', 'Enes'],
   },
 
   {
     page: 'projects',
     description:
       'Yıllar içinde çeşitli projelerde çalıştım ve kaydettiğim ilerlemeden gurur duyuyorum. Bu projelerin birçoğu açık kaynak kodludur ve başkalarının keşfetmesi ve katkıda bulunması için mevcuttur.',
-    keywords: ['Tharindu', 'Tharindu N', 'Tharindu Nayanajith'],
+    keywords: ['Enes Samed Gözlü', 'Enes Samed', 'Enes'],
   },
 
   {
     page: 'contact',
     description:
       'Bir projede işbirliği yapmakla ilgileniyorsanız, benimle iletişime geçmekten çekinmeyin. Her zaman yeni fikirler ve fırsatlara açığım.',
-    keywords: ['Tharindu', 'Tharindu N', 'Tharindu Nayanajith'],
+    keywords: ['Enes Samed Gözlü', 'Enes Samed', 'Enes'],
   },
 ];
 
