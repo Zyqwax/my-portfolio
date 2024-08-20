fc742aafdb4796aa081b670fa028675432dccf2a
Enes Samed <enessamed422@gmail.com>
2024-08-20 19:22:33 +0300

    fix sitemap file

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -1,5 +1,6 @@
+import articles from '@/data/articles';
+
 //pages/sitemap.xml.js
-const EXTERNAL_DATA_URL = 'https://jsonplaceholder.typicode.com/posts';
 
 function generateSiteMap(posts) {
   return `<?xml version="1.0" encoding="UTF-8"?>
@@ -15,7 +16,7 @@ function generateSiteMap(posts) {
        .map(({ id }) => {
          return `
        <url>
-           <loc>${`${EXTERNAL_DATA_URL}/${id}`}</loc>
+           <loc>${`enessamedgozlu.me/${id}`}</loc>
        </url>
      `;
        })
@@ -29,9 +30,7 @@ function SiteMap() {
 }
 
 export async function getServerSideProps({ res }) {
-  // We make an API call to gather the URLs for our site
-  const request = await fetch(EXTERNAL_DATA_URL);
-  const posts = await request.json();
+  const pages = ['', 'about', 'articles', 'contact', 'projects', ...articles.map((a) => a.slug)];
 
   // We generate the XML sitemap with the posts data
   const sitemap = generateSiteMap(posts);
